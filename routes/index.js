831131868cd1c1f71c98c6610f0e3af64ab9cb0e
Zyan <coderZyan@outlook.com>
2020-06-13 14:46:23 +0800

    获取票据

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,31 @@
-var express = require("express");
-var router = express.Router();
-const Wechat = require("./wechat");
+var express = require('express')
+var router = express.Router()
+const Wechat = require('./wechat')
 
-const config = require("../config/index");
+const config = require('../config/index')
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  console.log("aa");
-  res.render("index", { title: "Express" });
-});
-router.get("/checkSignature", (req, res, next) => {
-  let wxConfig = config.wxConfig;
-  const { signature, nonce, echostr, timestamp } = req.query;
-  let checkConfig = { signature, nonce, timestamp, token: wxConfig.token };
-  let wechat = new Wechat(checkConfig);
+router.get('/', function (req, res, next) {
+  console.log('aa')
+  res.render('index', { title: 'Express' })
+})
+router.get('/checkSignature', (req, res, next) => {
+  let wxConfig = config.wxConfig
+  const { signature, nonce, echostr, timestamp } = req.query
+  let checkConfig = { signature, nonce, timestamp, token: wxConfig.token }
+  let wechat = new Wechat(checkConfig)
   if (wechat.checkSignature()) {
-    res.end(echostr);
+    res.end(echostr)
   } else {
-    res.json({ msg: "微信签名未验证通过" });
+    res.json({ msg: '微信签名未验证通过' })
   }
-});
+})
+router.get('/initToken', (req, res, next) => {
+  let wxConfig = config.wxConfig
+  let wechat = new Wechat({ wxConfig: wxConfig })
+  wechat.getAccessTokenByFile().then((data) => {
+    res.json(data)
+  })
+})
 
-module.exports = router;
+module.exports = router
--- routes/wechat.js
+++ routes/wechat.js
@@ -1,26 +1,78 @@
-const sha1 = require("sha1");
+const sha1 = require('sha1')
+const fs = require('fs/promises')
+const path = require('path')
+const request = require('request')
 
 class Wechat {
   constructor(config) {
-    this.signature = config.signature;
-    this.timestamp = config.timestamp;
-    this.nonce = config.nonce;
-    this.token = config.token;
+    this.signature = config.signature
+    this.timestamp = config.timestamp
+    this.nonce = config.nonce
+    this.token = config.token
+    this.path = path.join(__dirname, '../config/token')
+    this.wxConfig = config.wxConfig
   }
   //与服务器验签
   checkSignature() {
-    let tmpArr = [this.token, this.timestamp, this.nonce];
-    tmpArr.sort();
-    let tmpStr = tmpArr.join("");
-    let sha = sha1(tmpStr);
+    let tmpArr = [this.token, this.timestamp, this.nonce]
+    tmpArr.sort()
+    let tmpStr = tmpArr.join('')
+    let sha = sha1(tmpStr)
 
     if (sha == this.signature) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   }
   //获取票据
+  getAccessTokenByFile() {
+    return fs
+      .readFile(this.path)
+      .then((res) => {
+        let token = JSON.parse(res)
+        if (!this.isValidAccessToken(token)) {
+          this.updateAccessToken()
+        } else {
+          return token
+        }
+      })
+      .catch((res) => {
+        this.updateAccessToken()
+      })
+  }
+  saveAccessTokenToFile(data) {
+    data = JSON.stringify(data)
+    fs.writeFile(this.path, data).then((res) => {
+      console.log('写入成功')
+    })
+  }
+  isValidAccessToken(data) {
+    let now = Date.now()
+    let locTime = data.expires_in
+    if (now > locTime || !data) {
+      return false
+    } else {
+      return true
+    }
+  }
+  updateAccessToken() {
+    request(
+      {
+        url: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${this.wxConfig.appId}&secret=${this.wxConfig.appsecret}`,
+        json: true,
+      },
+      (err, res, body) => {
+        if (err) {
+          console.log(err)
+        } else {
+          body.expires_in = Date.now() + (body.expires_in - 20) * 1000
+          console.log(body.expires_in)
+          this.saveAccessTokenToFile(body)
+        }
+      }
+    )
+  }
 }
 
-module.exports = Wechat;
+module.exports = Wechat
